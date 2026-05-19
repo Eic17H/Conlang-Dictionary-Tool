781e9462b578789b8fc63f20ac350414c1f35473
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2026-05-19 09:48:45 +0200

    Colpa mia

--- script/loadData.js
+++ script/loadData.js
@@ -123,7 +123,8 @@ function loadLanguage(path) {
             }
         }
         // Loading descendants
-        words[i].descendants = {lyz: {}, nus:{}}
+        for(let i in words)
+            words[i].descendants = {lyz: {}, nus:{}}
         return initializeSca2("lyz").then(() => {
             for(i in words) {
                 words[i]["descendants"]["lyz"] = runSCA(words[i].word)
